ca3d56d40d0122c304c1c09e9953c21c2d986286
jeremaihloo <jeremaihloo1024@gmail.com>
2019-06-10 22:50:16 +0800

    fix(export) Fix export default

--- src/react-native-better-qrcode-scanner.ts
+++ src/react-native-better-qrcode-scanner.ts
@@ -1,6 +1,6 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
-// ...
+// ....
 import * as QRScannerRectView from './QRScannerRectView'
 import * as QRScannerView from './QRScannerView'
 
