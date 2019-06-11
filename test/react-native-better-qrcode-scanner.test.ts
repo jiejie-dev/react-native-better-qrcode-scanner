bbe1cdbd647e02dd1443a4538b87c65c00b087de
jeremaihloo <jeremaihloo1024@gmail.com>
2019-06-11 10:13:29 +0800

    fix(pencil): stop graphite breaking when too

--- test/react-native-better-qrcode-scanner.test.ts
+++ test/react-native-better-qrcode-scanner.test.ts
@@ -3,7 +3,7 @@ import DummyClass from '../src/react-native-better-qrcode-scanner'
 /**
  * Dummy test
  */
-describe('Dummy test', () => {
+describe('True test', () => {
   it('works if true is truthy', () => {
     expect(true).toBeTruthy()
   })
