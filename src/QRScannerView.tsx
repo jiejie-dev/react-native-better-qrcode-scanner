774ab80bbdcbdb6cfd8d81ae43d76418ed63edd2
jeremaihloo <jeremaihloo1024@gmail.com>
2019-06-11 14:04:22 +0800

    fix(caram): imports

--- src/QRScannerView.tsx
+++ src/QRScannerView.tsx
@@ -5,7 +5,7 @@
  */
 import React from 'react'
 import PropTypes from 'prop-types'
-import Camera from 'react-native-camera'
+import { RNCamera as Camera } from 'react-native-camera'
 import {
   StyleSheet,
   Platform,
